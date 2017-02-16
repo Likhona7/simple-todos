cfac9dbf4ee00a63d0bf92fc28c52d007bafa79c
likhona7 <likhona7>
2017-02-16 11:59:10 -0500

    security added

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,14 +1,29 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-
-import { Tasks } from '../api/tasks.js'
-
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { Tasks } from '../api/tasks.js';
+import './task.js';
 import './body.html';
-Template.body.helpers({
 
+
+Template.body.onCreated(function bodyOnCreated(){
+this.state = new ReactiveDict();
+});
+
+
+Template.body.helpers({
 tasks() {
-  return Tasks.find({});
+  const instance = Template.instance();
+  if(instance.state.get('hideCompleted')){
+    return Tasks.find({ checked: {$ne: true } }, { sort: { createdAt: -1 } });
+  }
+  return Tasks.find({}, {sort: { createdAt: -1}});
+},
+incompleteCount(){
+  return Tasks.find({ checked: { $ne: true } }).count();
 },
 });
+
 Template.body.events({
   'submit .new-task'(event) {
         // Prevent default browser form submit
@@ -20,15 +35,20 @@ Template.body.events({
     Task.insert({
       text,
       createdAt : new Date(), //current time
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
+    
     target.text.value = '';
   },
+  'change .hide-complete input'(event, instance){
+    instance.state.set('hideCompleted', event.target.Checked);
+  },
 })
-Template.body.helpers({
 
+Template.body.helpers({
   tasks(){
     //show newest tasks at the top
     return Tasks.find({}, {sort: { createdAt: -1 } });
-
   },
 });
